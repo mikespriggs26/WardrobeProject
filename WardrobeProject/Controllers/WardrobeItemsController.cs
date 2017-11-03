1c905cd108ef847e174fd65aeade17724c6d0a1d
mikespriggs26 <mikespriggs@msn.com>
2017-11-03 18:43:44 -0400

    First commit

---
+++ WardrobeProject/Controllers/WardrobeItemsController.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WardrobeProject.Models;
+
+namespace WardrobeProject.Controllers
+{
+    public class WardrobeItemsController : Controller
+    {
+        private WardrobeProjectEntities db = new WardrobeProjectEntities();
+
+        // GET: WardrobeItems
+        public ActionResult Index()
+        {
+            var wardrobeItems = db.WardrobeItems.Include(w => w.Color).Include(w => w.ItemType).Include(w => w.Occasion).Include(w => w.Season);
+            return View(wardrobeItems.ToList());
+        }
+
+        // GET: WardrobeItems/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            WardrobeItem wardrobeItem = db.WardrobeItems.Find(id);
+            if (wardrobeItem == null)
+            {
+                return HttpNotFound();
+            }
+            return View(wardrobeItem);
+        }
+
+        // GET: WardrobeItems/Create
+        public ActionResult Create()
+        {
+            ViewBag.ColorID = new SelectList(db.Colors, "ColorID", "Color1");
+            ViewBag.ItemTypeID = new SelectList(db.ItemTypes, "ItemTypeID", "ItemType1");
+            ViewBag.OccasionID = new SelectList(db.Occasions, "OccasionID", "Occasion1");
+            ViewBag.SeasonID = new SelectList(db.Seasons, "SeasonID", "Season1");
+            return View();
+        }
+
+        // POST: WardrobeItems/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "WardrobeItemID,Name,Photo,SeasonID,ItemTypeID,ColorID,OccasionID")] WardrobeItem wardrobeItem)
+        {
+            if (ModelState.IsValid)
+            {
+                db.WardrobeItems.Add(wardrobeItem);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.ColorID = new SelectList(db.Colors, "ColorID", "Color1", wardrobeItem.ColorID);
+            ViewBag.ItemTypeID = new SelectList(db.ItemTypes, "ItemTypeID", "ItemType1", wardrobeItem.ItemTypeID);
+            ViewBag.OccasionID = new SelectList(db.Occasions, "OccasionID", "Occasion1", wardrobeItem.OccasionID);
+            ViewBag.SeasonID = new SelectList(db.Seasons, "SeasonID", "Season1", wardrobeItem.SeasonID);
+            return View(wardrobeItem);
+        }
+
+        // GET: WardrobeItems/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            WardrobeItem wardrobeItem = db.WardrobeItems.Find(id);
+            if (wardrobeItem == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.ColorID = new SelectList(db.Colors, "ColorID", "Color1", wardrobeItem.ColorID);
+            ViewBag.ItemTypeID = new SelectList(db.ItemTypes, "ItemTypeID", "ItemType1", wardrobeItem.ItemTypeID);
+            ViewBag.OccasionID = new SelectList(db.Occasions, "OccasionID", "Occasion1", wardrobeItem.OccasionID);
+            ViewBag.SeasonID = new SelectList(db.Seasons, "SeasonID", "Season1", wardrobeItem.SeasonID);
+            return View(wardrobeItem);
+        }
+
+        // POST: WardrobeItems/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "WardrobeItemID,Name,Photo,SeasonID,ItemTypeID,ColorID,OccasionID")] WardrobeItem wardrobeItem)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(wardrobeItem).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.ColorID = new SelectList(db.Colors, "ColorID", "Color1", wardrobeItem.ColorID);
+            ViewBag.ItemTypeID = new SelectList(db.ItemTypes, "ItemTypeID", "ItemType1", wardrobeItem.ItemTypeID);
+            ViewBag.OccasionID = new SelectList(db.Occasions, "OccasionID", "Occasion1", wardrobeItem.OccasionID);
+            ViewBag.SeasonID = new SelectList(db.Seasons, "SeasonID", "Season1", wardrobeItem.SeasonID);
+            return View(wardrobeItem);
+        }
+
+        // GET: WardrobeItems/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            WardrobeItem wardrobeItem = db.WardrobeItems.Find(id);
+            if (wardrobeItem == null)
+            {
+                return HttpNotFound();
+            }
+            return View(wardrobeItem);
+        }
+
+        // POST: WardrobeItems/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            WardrobeItem wardrobeItem = db.WardrobeItems.Find(id);
+            db.WardrobeItems.Remove(wardrobeItem);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ WardrobeProject/Models/WardrobeProjectContext.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace WardrobeProject.Models
+{
+    public class WardrobeProjectContext : DbContext
+    {
+        // You can add custom code to this file. Changes will not be overwritten.
+        // 
+        // If you want Entity Framework to drop and regenerate your database
+        // automatically whenever you change your model schema, please use data migrations.
+        // For more information refer to the documentation:
+        // http://msdn.microsoft.com/en-us/data/jj591621.aspx
+    
+        public WardrobeProjectContext() : base("name=WardrobeProjectContext")
+        {
+        }
+
+        public System.Data.Entity.DbSet<WardrobeProject.Models.WardrobeItem> WardrobeItems { get; set; }
+
+        public System.Data.Entity.DbSet<WardrobeProject.Models.Color> Colors { get; set; }
+
+        public System.Data.Entity.DbSet<WardrobeProject.Models.ItemType> ItemTypes { get; set; }
+
+        public System.Data.Entity.DbSet<WardrobeProject.Models.Occasion> Occasions { get; set; }
+
+        public System.Data.Entity.DbSet<WardrobeProject.Models.Season> Seasons { get; set; }
+    }
+}
